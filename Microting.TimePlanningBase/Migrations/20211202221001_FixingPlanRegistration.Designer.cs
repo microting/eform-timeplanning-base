d2db89c42abc0dc4d8c67e43260daf8777e2fcd2
René Schultz Madsen <rm@microting.com>
2021-12-02 22:10:30 +0000

    Fixing PlanRegistration.

--- Microting.TimePlanningBase/Migrations/20211202221001_FixingPlanRegistration.Designer.cs
+++ Microting.TimePlanningBase/Migrations/20211202221001_FixingPlanRegistration.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Microting.TimePlanningBase.Infrastructure.Data;
 
@@ -10,9 +11,10 @@ using Microting.TimePlanningBase.Infrastructure.Data;
 namespace Microting.TimePlanningBase.Migrations
 {
     [DbContext(typeof(TimePlanningPnDbContext))]
-    partial class TimePlanningPnDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211202221001_FixingPlanRegistration")]
+    partial class FixingPlanRegistration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Microting.TimePlanningBase/Migrations/20211202221001_FixingPlanRegistration.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Microting.TimePlanningBase.Migrations
+{
+    public partial class FixingPlanRegistration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PlanRegistrations_AssignedSites_AssignedSiteId",
+                table: "PlanRegistrations");
+
+            migrationBuilder.RenameColumn(
+                name: "AssignedSiteId",
+                table: "PlanRegistrationVersions",
+                newName: "SdkSitId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AssignedSiteId",
+                table: "PlanRegistrations",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "SdkSitId",
+                table: "PlanRegistrations",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PlanRegistrations_AssignedSites_AssignedSiteId",
+                table: "PlanRegistrations",
+                column: "AssignedSiteId",
+                principalTable: "AssignedSites",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PlanRegistrations_AssignedSites_AssignedSiteId",
+                table: "PlanRegistrations");
+
+            migrationBuilder.DropColumn(
+                name: "SdkSitId",
+                table: "PlanRegistrations");
+
+            migrationBuilder.RenameColumn(
+                name: "SdkSitId",
+                table: "PlanRegistrationVersions",
+                newName: "AssignedSiteId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AssignedSiteId",
+                table: "PlanRegistrations",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PlanRegistrations_AssignedSites_AssignedSiteId",
+                table: "PlanRegistrations",
+                column: "AssignedSiteId",
+                principalTable: "AssignedSites",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
