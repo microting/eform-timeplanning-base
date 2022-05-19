82b5032f8590d612b78ffd323ad6875548910389
René Schultz Madsen <rm@microting.com>
2022-05-19 17:47:19 +0200

    Adding the new TimePlanningBaseSettings to Models.

---
+++ Microting.TimePlanningBase/Infrastructure/Data/Models/TimePlanningBaseSettings.cs
@@ -0,0 +1,14 @@
+namespace Microting.TimePlanningBase.Infrastructure.Data.Models;
+
+public class TimePlanningBaseSettings
+{
+    public int? EformId{ get; set; }
+
+    public int? FolderId { get; set; }
+
+    public int? InfoeFormId { get; set; }
+
+    public int? MaxHistoryDays { get; set; }
+
+    public int? MaxDaysEditable { get; set; }
+}
