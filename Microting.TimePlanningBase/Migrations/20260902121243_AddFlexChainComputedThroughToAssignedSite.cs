b4c5116351f2b419d84f994369f4279768156b13
René Schultz Madsen <rm@microting.dk>
2026-09-02 14:13:14 +0200

    feat(schema): add AssignedSite.FlexChainComputedThrough

---
+++ Microting.TimePlanningBase/Migrations/20260902121243_AddFlexChainComputedThroughToAssignedSite.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Microting.TimePlanningBase.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFlexChainComputedThroughToAssignedSite : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FlexChainComputedThrough",
+                table: "AssignedSiteVersions",
+                type: "datetime(6)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FlexChainComputedThrough",
+                table: "AssignedSites",
+                type: "datetime(6)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FlexChainComputedThrough",
+                table: "AssignedSiteVersions");
+
+            migrationBuilder.DropColumn(
+                name: "FlexChainComputedThrough",
+                table: "AssignedSites");
+        }
+    }
+}
